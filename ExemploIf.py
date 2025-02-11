ab9ac7612e1880010b46c271e7365a22715dc85a
Isaac <isaac_s_souza@estudante.sesisenai.org.br>
2025-02-10 22:15:31 -0300

    EXE

---
+++ ExemploIf.py
@@ -0,0 +1,7 @@
+num1 = int(input('Qual numero voce deseja?: '))
+if num1 < 10:
+    print("Seu numero é menor que 10")
+if num1 == 10:
+    print("Seu numero é igual que 10")
+if num1 > 10:
+    print("Seu numero é maior que 10")
